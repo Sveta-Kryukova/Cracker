bfa0d189ecc41e6b39b8fc60a825ed5c47b221e4
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-07-28 20:49:46 +0300

    added functionality redux store save in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-
-import { App } from './App.tsx'
-
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store';
 import store from './store'
+import { App } from './App.tsx';
 
-import './main.scss'
+import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,19 @@
 import { Dispatch, configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from '../types'; 
+import { RootState } from '../types';
 import { cartSlice } from './cart';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSlice.reducer);
 
 const store = configureStore({
-  reducer: cartSlice.reducer,
+  reducer: persistedReducer,
 });
 
 export default store;
@@ -12,3 +21,5 @@ export default store;
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useAppDispatch = () => useDispatch<Dispatch>();
+
+export const persistor = persistStore(store);
